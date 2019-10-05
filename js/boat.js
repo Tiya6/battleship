e809a70d5ddc2f12476a5d620dbedbbe3a7ff71f
Patri <tiyaseiss@gmail.com>
2019-10-05 20:37:37 +0200

    Styles

--- js/boat.js
+++ js/boat.js
@@ -4,17 +4,8 @@ class Boat {
   }
 
   deleteBoat(){
-    let del = document.getElementsByClassName('water-tile')
-    console.log(del)
-    
-    /*for(let i = 0; i < del.length; i++){
-      let delBt = document.getElementsByClassName('boat')
-      let counter = 0;
-      if(delBt==='boat'){
-        counter+=1
-        console.log(counter)
-      }
-      }*/
-    }
+    let del = document.getElementsByClassName('boat')
+    console.log(del.inerText)
+  }
     
   }
